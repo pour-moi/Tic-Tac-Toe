5c046312012c0a709705a416d79cc4a3d22b5256
pour-moi <pour64moi@gmail.com>
2023-09-20 17:32:02 +0300

    making the computer to choose

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let boxes = document.querySelectorAll(".box");
 
+function getComputerChoice(boxes) {
+  let availableBoxes = Array.from(boxes).filter(
+    (box) => box.querySelector(".xo").textContent === ""
+  );
+  return availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
+}
+
 boxes.forEach(function (box) {
   box.addEventListener("click", function () {
     let xo = this.querySelector(".xo");
-    xo.textContent = "x";
+    if (xo.textContent === "") {
+      xo.textContent = "X";
+
+      let computerBox = getComputerChoice(boxes);
+      if (computerBox) {
+        let computerXO = computerBox.querySelector(".xo");
+        computerXO.textContent = "o";
+      }
+    }
   });
 });
